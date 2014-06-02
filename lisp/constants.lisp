79d558898944d26ad7dbca4c7608fb78a757a626
Thomas M. Hermann <thomas.m.hermann@odonata-research.com>
2014-06-02 09:06:44 -0500

    Update the copyright for the tests.

--- lisp/constants.lisp
+++ lisp/constants.lisp
@@ -2,7 +2,7 @@
 
   Floating Point
 
-  Copyright (c) 2013 Odonata Research LLC
+  Copyright (c) 2013-2014, Odonata Research LLC
 
   Permission is hereby granted, free  of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
